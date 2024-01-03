4a720c62f6079d63dd2d32768babfbd472280234
GUO YANKE <hi@guoyk.xyz>
2024-01-03 11:49:21 +0800

    fix: fix dist.sh

--- dist.sh
+++ dist.sh
@@ -6,24 +6,26 @@ cd "$(dirname "${0}")"
 
 rm -rf dist && mkdir dist
 
+EXECUTABLE_NAME="imapdump"
+
 build() {
   rm -rf build && mkdir build
-  GOOS=${1} GOARCH=${2} go build -o build/imapdump ./cmd/imapdump
-  tar -czvf "dist/imapdump-${1}-${2}.tar.gz" --exclude ".*" -C build imapdump
+  GOOS=${1} GOARCH=${2} go build -o "build/${EXECUTABLE_NAME}${3}" "./cmd/${EXECUTABLE_NAME}"
+  tar -czvf "dist/${EXECUTABLE_NAME}-${1}-${2}.tar.gz" --exclude ".*" -C build "${EXECUTABLE_NAME}${3}"
   rm -rf build
 }
 
-build linux amd64
-build windows amd64
-build darwin amd64
-build darwin arm64
+build linux amd64 ""
+build windows amd64 ".exe"
+build darwin amd64 ""
+build darwin arm64 ""
 
-build linux arm64
-build linux loong64
-build windows 386
-build freebsd amd64
+build linux arm64 ""
+build linux loong64 ""
+build windows 386 ".exe"
+build freebsd amd64 ""
 
 cd dist
 
-shasum -a 256 *.tar.gz > SHASUM256.txt
+shasum -a 256 *.tar.gz >SHASUM256.txt
 gpg-trezor-hi@guoyk.xyz -ab SHASUM256.txt
